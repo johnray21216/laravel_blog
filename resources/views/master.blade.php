9d9879aede4ae98aae927f303efa7d9a54c4b169
John Ray Hernani <johnray21216@gmail.com>
2018-09-07 14:24:54 +0800

    additional files from exam

---
+++ resources/views/master.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<!--[if lt IE 9]>
+<html lang="ja" class="no-js lt-ie9" prefix="og: http://ogp.me/ns#">
+<![endif]-->
+<!--[if gt IE 9]><!-->
+<html lang="ja" class="no-js" prefix="og: http://ogp.me/ns#"><!--<![endif]-->
+<head>
+  <meta charset="UTF-8">
+  <title>Title</title>
+  <meta name="description" content="sample text,sample tex">
+  <meta name="keywords" content="word1,word2,word3">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta property="og:title" content="">
+  <meta property="og:description" content="sample text,sample tex">
+  <meta property="og:url" content="">
+  <meta property="og:site_name" content="">
+  <meta property="og:type" content="blog">
+  <meta property="fb:admins" content="">
+  <meta property="og:image" content="assets/images/common/ogp.png">
+
+  <meta name="apple-mobile-web-app-title" content="">
+
+  <link rel="shortcut icon" href="assets/images/common/favicon.ico">
+  <link rel="apple-touch-icon-precomposed" href="images/common/apple-touch-icon-precomposed.png">
+  <link rel="stylesheet" href="assets/css/style.css">
+
+  <script src="assets/lib/modernizr.js"></script>
+</head>
+<body id="js-body">
+@yield('content')
+<!--footer ここから-->
+<footer class="l-footer ">
+
+   <div class="l-footer-button">
+   <a class="page-top js-scroll" href="#js-body">
+       <span class="page-top-arrow"></span>
+   </a>
+   </div>
+   <div class="l-footer-copyright">
+       <small class="copyright">&copy;copyright</small>
+   </div>
+
+</footer>
+<!--footer ここまで-->
+<!--javascript ここから-->
+<script src="./assets/lib/jquery-3.1.1.min.js"></script>
+<script src="./assets/js/vendor.js"></script>
+<script src="./assets/js/app.js"></script>
+<!--javascript ここまで-->
+@yield('scripts')
+</body>
+</html>
